7bec9f750cb37fabb23c8466b70b0708dcca642b
SumitBaria005 <2018itsum087@ldce.ac.in>
2022-02-12 11:07:29 +0530

    initial Config Commit

---
+++ digitalDen/src/main/java/com/example/digitalDen/DigitalDenApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalDen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalDenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalDenApplication.class, args);
+	}
+
+}
---
+++ digitalDen/src/test/java/com/example/digitalDen/DigitalDenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalDen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalDenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
