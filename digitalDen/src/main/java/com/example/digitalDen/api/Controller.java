875374ecd3bec0aa136f77f3ec7b0d25e44fdf6c
SumitBaria005 <2018itsum087@ldce.ac.in>
2022-02-14 16:47:22 +0530

    Added impl file in service

--- digitalDen/src/main/java/com/example/digitalDen/api/Controller.java
+++ digitalDen/src/main/java/com/example/digitalDen/api/Controller.java
@@ -1,6 +1,6 @@
 package com.example.digitalDen.api;
 
-import com.example.digitalDen.services.Services;
+import com.example.digitalDen.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     @Autowired
-    private Services services;
+    private ProductService productService;
 
 }
---
+++ digitalDen/src/main/java/com/example/digitalDen/services/impl/ProductServiceImplementation.java
@@ -0,0 +1,15 @@
+package com.example.digitalDen.services.impl;
+
+import com.example.digitalDen.entities.Product;
+import com.example.digitalDen.services.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+class ProductServiceImplementation implements ProductService {
+    @Override
+    public List<Product> getProducts(int pageNo) {
+        return null;
+    }
+}
