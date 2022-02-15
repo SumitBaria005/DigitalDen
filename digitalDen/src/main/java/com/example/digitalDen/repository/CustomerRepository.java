f0fae6ea660dd8c74b4e333bbb226eb247e794c3
sharmabhumi2000 <sharmabhumi2000@gmail.com>
2022-02-15 16:21:38 +0530

    Update and Delete of Customers

---
+++ digitalDen/src/main/java/com/example/digitalDen/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.example.digitalDen.repository;
+
+import com.example.digitalDen.entities.Customer;
+import org.springframework.http.ResponseEntity;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CustomerRepository {
+    List<Customer> getCustomersData() throws SQLException;
+    Customer getCustomerData(Integer customerId) throws SQLException;
+    ResponseEntity<String> getCustomerLoginData(String email, String password) throws SQLException;
+    ResponseEntity<String>  setCustomerData(Customer customer) throws SQLException;
+    ResponseEntity<String>  updateCustomerData(Customer customer) throws SQLException;
+}
