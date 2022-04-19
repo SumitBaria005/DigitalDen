68415f1b8b56b7bcf8fefee8114d032d9b277543
SumitBaria005 <2018itsum087@ldce.ac.in>
2022-04-19 11:26:39 +0530

    Added Bean for CORS

--- digitalDen/src/main/java/com/example/digitalDen/DigitalDenApplication.java
+++ digitalDen/src/main/java/com/example/digitalDen/DigitalDenApplication.java
@@ -2,6 +2,9 @@ package com.example.digitalDen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
@@ -10,4 +13,17 @@ public class DigitalDenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalDenApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
+	}
+
 }
